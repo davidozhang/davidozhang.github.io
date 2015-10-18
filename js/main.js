0e163e1eb7866df5bb13a9f7f408df88c383d3d9
David Zhang <davzee@hotmail.com>
2015-10-18 15:32:14 -0400

    Significantly improve mobile experience

--- js/main.js
+++ js/main.js
@@ -17,6 +17,27 @@ $(function() {
     });
 });
 
+$(function($,W,D) {
+     var backgroundResize = {
+         init: function() {
+            var update = function(e) {
+                var bg = (window.innerWidth > 1024) ? 
+                "/assets/img/background.jpg" 
+                : "/assets/img/background-small-screen.jpg";
+                $.backstretch(bg);
+            };
+            $(window).resize(function() {
+                update();
+            });
+            update();
+         }
+     };
+
+     $(function() {
+        backgroundResize.init();
+     });
+})(jQuery, window, document);
+
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
