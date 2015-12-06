68e53f3de868e7eb60a83349cb379dbbc263a89f
David Zhang <davzee@hotmail.com>
2015-12-06 12:34:47 -0500

    Change offset for navigation bar collapse

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 600) {
+    if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
